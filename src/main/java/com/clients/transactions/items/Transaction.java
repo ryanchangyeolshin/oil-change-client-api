63ee41f9b6746dc3bd47442fcd09d6aabb93bd32
Ryan Shin <changyeolshin@gmail.com>
2021-04-05 08:07:49 +0900

    Create the Transaction item.

---
+++ src/main/java/com/clients/transactions/items/Transaction.java
@@ -0,0 +1,76 @@
+package com.clients.transactions.items;
+
+import com.clients.client.items.Client;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+public class Transaction {
+    private @Id @GeneratedValue Long id;
+
+    @ManyToOne
+    @JoinColumn(name="client_id")
+    private Client client;
+
+    private int amount;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Transaction () { }
+
+    public Transaction(Long id, Client client, int amount) {
+        this.id = id;
+        this.client = client;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
